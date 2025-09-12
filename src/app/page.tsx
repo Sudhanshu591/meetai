4a61e6c7ee6234a1ab31840f23053411f853bcb6
Sudhanshu591 <tambolisudhanshus@gmail.com>
2025-09-12 15:45:33 +0530

    3: better auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,42 @@
+"use client";
+import { useState } from "react";
+import { authClient } from "@/lib/auth-client"; //import the auth client
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
+  const onSubmit = () => { 
+    authClient.signUp.email({
+      email,
+      name,
+      password,
+    }, {
+      onError: () => {      
+        window.alert("Something went wrong");
+      },
+      onSuccess: () => {
+        window.alert("Success");
+      }
+    });
+
+  }
+  
+  
   return (
-  <Button>
-    Click me
+  <div className="p-4 flex flex-col gap-y-4">
+    <Input placeholder="name" value={name} onChange={(e) => setName (e.target.value)} />
+    <Input placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+    <Input placeholder="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+  
+  <Button onClick={onSubmit}>
+    create user
   </Button>
+  
+  </div>
   )
 }
   
---
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { Card } from "@/components/ui/card";
+
+export const SignUpView = () => {
+    // console.log("Sign up view")
+
+    return (
+        <Card>
+            Sign up view
+        </Card>
+    );
+};
+
+ 
